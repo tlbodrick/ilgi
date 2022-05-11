ceffaf7754f8aa0dd43373fdfa581a6abaf4f814
Tamera <tlbodrick@gmail.com>
2022-05-11 18:19:24 -0400

    add sign out functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,31 @@
 import Entry from "../components/Entry"
+import { useNavigate } from 'react-router-dom'
+import { signOut, getAuth } from 'firebase/auth'
+import { toast } from 'react-toastify'
 
 function Home() {
+
+    const navigate = useNavigate()
+
+    const signUserOut = () => {
+        const auth = getAuth()
+        try {
+            signOut(auth)
+            navigate('/sign-in')
+        } catch (error) {
+            toast.error('Sign out failed.')
+        }
+    }
+
     return (
-        <div className="flex">
-            <Entry />
-        </div>
+        <>
+            <header className="container">
+                <p className="cursor-pointer absolute right-0 mt-5 mr-5" onClick={signUserOut}>Sign out</p>
+            </header>
+            <div className="flex">
+                <Entry />
+            </div>
+        </>
 
     )
 }
