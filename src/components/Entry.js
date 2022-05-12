845867b82ad8e2e49d037c863f174f5504f2edad
Tamera <tlbodrick@gmail.com>
2022-05-11 22:52:01 -0400

    create and add entries to firestore

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,11 +1,42 @@
+import { useState, useEffect } from 'react'
 import { promptList } from "./prompts.js"
 import { IconContext } from 'react-icons'
 import { BsPencilSquare, BsTrash2 } from 'react-icons/bs'
 import { VscSave } from 'react-icons/vsc'
-
+import { getAuth } from 'firebase/auth'
+import { doc, setDoc } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
 
 
 function Entry() {
+    const [prompt, setPrompt] = useState('')
+    const [entryText, setEntryText] = useState('')
+
+    useEffect(() => {
+        setPrompt(promptList[random()])
+    }, [])
+
+    const onChange = (e) => {
+        setEntryText(e.target.value)
+    }
+
+    const addEntry = async () => {
+        try {
+            const auth = getAuth()
+            const user = auth.currentUser
+            await setDoc(doc(db, "entries", user.uid), {
+                prompt, entryText,
+                userId: user.uid,
+            })
+            toast.info('post saved')
+        } catch (error) {
+            toast.error('Could not save post')
+            console.log(error)
+        }
+
+    }
+
     const date = new Date();
     const year = date.getFullYear()
     const month = date.getMonth()
@@ -15,6 +46,7 @@ function Entry() {
     const random = () => {
         return (year * day * (month + 1)) % promptList.length;
     }
+
     return (
         <div className="flex w-screen h-screen items-center justify-center">
             <div className="border p-10 w-2/3">
@@ -23,7 +55,7 @@ function Entry() {
                     <div className="flex gap-3">
                         <IconContext.Provider value={{ className: "cursor-pointer" }}>
                             <BsPencilSquare />
-                            <VscSave />
+                            <VscSave onClick={addEntry} />
                             <BsTrash2 />
                         </IconContext.Provider>
                     </div>
@@ -32,8 +64,13 @@ function Entry() {
                 </div>
 
                 <div>
-                    <p className="pb-4">{promptList[random()]}</p>
-                    <textarea className="border w-full h-60"></textarea>
+                    <p className="pb-4">{prompt}</p>
+                    <textarea
+                        value={entryText}
+                        onChange={onChange}
+                        className="border w-full h-60"
+                    >
+                    </textarea>
                 </div>
             </div>
         </div >
